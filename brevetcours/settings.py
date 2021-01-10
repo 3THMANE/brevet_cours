050a30fe818111d8ff71daacae9a4c8efb6c7b5d
3THMANE <elhasseneyahoo@gmail.com>
2021-01-09 20:03:14 -0800

    modif

--- brevetcours/settings.py
+++ brevetcours/settings.py
@@ -82,16 +82,17 @@ WSGI_APPLICATION = 'brevetcours.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'USER':''
-        #'PORT':''
-        #'HOST':''
-        #'PASSWORD':''
+        'ENGINE': 'django.db.backends.ec2-3-231-241-17.compute-1.amazonaws.com',
+        'NAME':'d2p3p392fhql09',
+        'USER':'adayhrsmtscihw'
+        'PORT':'5432'
+        # 'HOST':'ec2-3-231-241-17.compute-1.amazonaws.com'
+        'PASSWORD':'975266f7c2e132f9ab390a6f357e6029fb01a9629afa3775e46cdad5c9ce0439'
     }
 }
 
-
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
