40fa2c0f338c3a1d3ca38535d312c8f2e50207ae
3THMANE <elhasseneyahoo@gmail.com>
2021-03-03 19:05:53 -0800

    make it better

--- brevetcours/settings.py
+++ brevetcours/settings.py
@@ -51,7 +51,12 @@ INSTALLED_APPS = [
     # 'jet',  
     'import_export',
 ]
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -88,12 +93,12 @@ WSGI_APPLICATION = 'brevetcours.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # DEVELOPMENT_MDE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
@@ -117,17 +122,16 @@ DATABASES = {
 # DATABASE_URL = os.environ['DATABASE_URL']
 
 # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME' : 'd19dkfjuh8r65m',
-#         'USER' : 'bsjotpwowfswnz',
-#         'PORT' : '5432',
-#         'PASSWORD' : 'e0b758c277989aa6fe3179d7314ca4fdd41d89abbdd424f98202dd7c4672b2c3',
-#         'HOST' : 'ec2-54-85-13-135.compute-1.amazonaws.com',
-        
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
+    }
+}
 
 # import dj_database_url
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
